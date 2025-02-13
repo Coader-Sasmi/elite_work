4aa483ca6bb93d860e02cf83a8f92d5a3db7b630
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-02-14 00:55:57 +0530

    Our Services design ongoing

--- src/components/ChooseUs.tsx
+++ src/components/ChooseUs.tsx
@@ -54,76 +54,78 @@ export default function ChooseUs() {
     },
   ];
   return (
-    <aside className="main-container py-10 flex lg:flex-row items-center flex-col gap-10">
-      <section className="w-full flex flex-col gap-10 pt-16 md:pt-0">
-        <motion.div
-          className="flex gap-3 items-center text-quaternary"
-          initial={{ y: 50, opacity: 0.5 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 1.5 }}
-        >
-          <CgArrowLongRightL />
-          <h5 className="font-medium">Why Choose Us</h5>
-        </motion.div>
-        <h1 className="text-4xl font-semibold">
-          A behind the scenes look at
-          <br /> <span className="text-quaternary">our agency</span>
-        </h1>
-        <p>
-          From concept to completion, discover how we bring your vision to life
-          with innovation, collaboration, and expert craftsmanship.
-        </p>
-        {ChooseUsArr?.map((curElm, i) => (
+    <aside className="bg-gray-50 ">
+      <aside className="main-container py-10 flex lg:flex-row items-center flex-col gap-10">
+        <section className="w-full flex flex-col gap-10 pt-16 md:pt-0">
           <motion.div
-            key={i}
-            className="flex md:flex-row flex-col-reverse gap-5 justify-between w-full"
+            className="flex gap-3 items-center text-quaternary"
             initial={{ y: 50, opacity: 0.5 }}
             whileInView={{ y: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1.5, delay: 0.5 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
           >
-            <div className="flex flex-col gap-8">
-              <div className="flex items-center gap-5">
-                <motion.div
-                  whileHover="hover"
-                  className="relative rounded-full overflow-hidden p-6 bg-gray-200 "
-                >
+            <CgArrowLongRightL />
+            <h5 className="font-medium">Why Choose Us</h5>
+          </motion.div>
+          <h1 className="text-4xl font-semibold">
+            A behind the scenes look at
+            <br /> <span className="text-quaternary">our agency</span>
+          </h1>
+          <p>
+            From concept to completion, discover how we bring your vision to
+            life with innovation, collaboration, and expert craftsmanship.
+          </p>
+          {ChooseUsArr?.map((curElm, i) => (
+            <motion.div
+              key={i}
+              className="flex md:flex-row flex-col-reverse gap-5 justify-between w-full"
+              initial={{ y: 50, opacity: 0.5 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 1.5, delay: 0.5 }}
+            >
+              <div className="flex flex-col gap-8">
+                <div className="flex items-center gap-5">
                   <motion.div
-                    className="absolute inset-0 bg-secondary "
-                    initial={{ width: "0%" }}
-                    variants={{
-                      hover: {
-                        width: "100%",
-                        transition: { duration: 0.4, ease: "easeInOut" },
-                      },
-                    }}
+                    whileHover="hover"
+                    className="relative rounded-full overflow-hidden p-6 bg-gray-200 "
                   >
-                    <p className="relative z-10 p-3 text-quaternary">
-                      {curElm?.icon}
-                    </p>
+                    <motion.div
+                      className="absolute inset-0 bg-secondary "
+                      initial={{ width: "0%" }}
+                      variants={{
+                        hover: {
+                          width: "100%",
+                          transition: { duration: 0.4, ease: "easeInOut" },
+                        },
+                      }}
+                    >
+                      <p className="relative z-10 p-3 text-quaternary">
+                        {curElm?.icon}
+                      </p>
+                    </motion.div>
                   </motion.div>
-                </motion.div>
-                <div className="flex flex-col gap-1">
-                  <h1 className="text-lg font-semibold">{curElm?.title}</h1>
-                  <p>{curElm?.subTitle}</p>
+                  <div className="flex flex-col gap-1">
+                    <h1 className="text-lg font-semibold">{curElm?.title}</h1>
+                    <p>{curElm?.subTitle}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </motion.div>
-        ))}
-      </section>
-      <section className="w-full">
-        <div className="grid grid-cols-12 gap-5">
-          {ImgArr?.map((curElm, i) => (
-            <div key={i} className={`${curElm?.class}`}>
-              <img
-                src={curElm?.img}
-                alt="image"
-                className={`w-auto h-auto ${curElm?.imgC}`}
-              />
-            </div>
+            </motion.div>
           ))}
-        </div>
-      </section>
+        </section>
+        <section className="w-full">
+          <div className="grid grid-cols-12 gap-5">
+            {ImgArr?.map((curElm, i) => (
+              <div key={i} className={`${curElm?.class}`}>
+                <img
+                  src={curElm?.img}
+                  alt="image"
+                  className={`w-auto h-auto ${curElm?.imgC}`}
+                />
+              </div>
+            ))}
+          </div>
+        </section>
+      </aside>
     </aside>
   );
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,70 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
+import { motion } from "framer-motion";
+import { BsArrowRight } from "react-icons/bs";
+import { CgArrowLongRightL } from "react-icons/cg";
+
 export default function Services() {
-  const ServiceArr = [
-    {
-      key: "1",
-      title: "Conceptual Design",
-      desc: "Developing initial design concepts and ideas based on client input and project parameters.",
-      subDes1: "Feasibility Studies",
-      subDes2: "Conceptual Design",
-      subDes3: "Design Development",
-    },
-    {
-      key: "2",
-      title: "Schematic Design",
-      desc: "Creating preliminary sketches and diagrams to explore design options and spatial relationships.",
-      subDes1: "Conceptualization",
-      subDes2: "Spatial Planning",
-      subDes3: "Client Collaboration",
-    },
-    {
-      key: "3",
-      title: "Interior Design",
-      desc: "Planning and designing interior spaces, selecting finishes, furnishings, and fixtures to create functional.",
-      subDes1: "Space Planning",
-      subDes2: "Aesthetics and Style",
-      subDes3: "Project Management",
-    },
-  ];
   return (
     <aside className="main-container py-10 flex flex-col gap-10">
-      <section className="flex flex-col gap-6 justify-center items-center">
-        <div className="flex justify-center items-center">
-          <div className="md:w-32 w-16 h-[0.01rem] bg-gray-400"></div>
-          <div className="text-gray-400 px-4 uppercase">Services</div>
-          <div className="md:w-32 w-16 h-[0.01rem] bg-gray-400"></div>
+      <section className="w-full flex flex-col gap-10">
+        <motion.div
+          className="flex gap-3 items-center text-quaternary"
+          initial={{ y: 50, opacity: 0.5 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 1.5 }}
+        >
+          <CgArrowLongRightL />
+          <h5 className="font-medium">Our Services</h5>
+        </motion.div>
+        <div className="flex lg:flex-row flex-col gap-8">
+          <h1 className="text-4xl font-semibold w-full">
+            Innovative design services for
+            <br /> <span className="text-quaternary">every need</span>
+          </h1>
+          <p className="w-full">
+            We offer a range of bespoke interior design services tailored to
+            your unique needs. From concept development to final installation.
+          </p>
         </div>
-        <h1 className="text-center md:text-6xl text-4xl font-semibold">
-          Expertly crafting
-          <br /> your dream
-        </h1>
-        <p className="text-center text-gray-700">
-          We turn your interior design dreams into reality with our specialized
-          touch in elegant and <br /> functional spaces.
-        </p>
       </section>
-      <section className="flex flex-col gap-5 py-10">
-        {ServiceArr?.map((curElm) => (
-          <div
-            key={curElm?.key}
-            className={`grid lg:grid-cols-12 gap-5 lg:place-items-center hover:bg-primary/5 p-5 ${
-              curElm?.key === "2" ? "bg-primary/10" : ""
-            }`}
+      <section className="flex justify-center items-center">
+        <motion.div
+          whileHover="hover"
+          className="relative w-48 cursor-pointer overflow-hidden p-5 bg-quaternary"
+        >
+          <motion.div
+            className="absolute inset-0 bg-secondary whitespace-nowrap"
+            initial={{ width: "0%" }}
+            variants={{
+              hover: {
+                width: "100%",
+                transition: { duration: 0.4, ease: "easeInOut" },
+              },
+            }}
           >
-            <h1 className="lg:col-span-4 text-start text-lg font-semibold">
-              {curElm?.title}
-            </h1>
-            <p className="lg:col-span-4 text-start text-gray-500">
-              {curElm?.desc}
-            </p>
-            <div className="lg:col-span-4 flex flex-col lg:flex-col md:flex-row lg:gap-1 gap-1 md:gap-5">
-              <p className="font-semibold">+ {curElm?.subDes1}</p>
-              <p className="font-semibold">+ {curElm?.subDes2}</p>
-              <p className="font-semibold">+ {curElm?.subDes3}</p>
-            </div>
-          </div>
-        ))}
+            <button className="relative z-10 flex gap-3 items-center font-semibold px-6 text-white py-2">
+              See All Services <BsArrowRight />
+            </button>
+          </motion.div>
+        </motion.div>
       </section>
     </aside>
   );
